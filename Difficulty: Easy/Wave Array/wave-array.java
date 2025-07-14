dfef840c18acd727b0d328e75bf3e03c5771e600
Gokul Sahoo <gokulsahoopc@gmail.com>
2025-07-14 14:14:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Wave Array/wave-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void sortInWave(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i += 2) {
+            // Swap arr[i] and arr[i+1]
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+}
